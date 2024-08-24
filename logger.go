d7347eadb502feffa6ddf0b4c3bbe04ef2e6033e
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-08-24 19:16:06 +0200

    Update object key

--- logger.go
+++ logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
@@ -35,10 +36,22 @@ func NewLogger(cfg *loggerConfig.Config) (*Logger, error) {
 }
 
 func (l *Logger) UploadLog(ctx context.Context, entry logentry.LogEntry) error {
-	objectKey := entry.CorrelationID
-	logData, err := json.Marshal(entry)
-	if err != nil {
-		return err
-	}
-	return s3.UploadLog(ctx, l.s3Client, l.bucketName, objectKey, logData)
+    objectKey := fmt.Sprintf("%s%s%s%s%s%s%s%s%s%s",
+	    entry.ServiceName + "/",
+        fmt.Sprintf("%02d/", entry.Timestamp.Year()),
+		fmt.Sprintf("%02d/", entry.Timestamp.Month()), 
+		fmt.Sprintf("%02d/", entry.Timestamp.Day()),
+        entry.LogLevel + "/",
+        entry.CorrelationID + "/",
+        entry.HTTPMethod + "/",
+		entry.URL + "/",
+        fmt.Sprintf("%v/", entry.ResponseStatus),
+		fmt.Sprintf("%v/", entry.ExecutionTime),
+    )
+
+    logData, err := json.Marshal(entry)
+    if err != nil {
+        return err
+    }
+    return s3.UploadLog(ctx, l.s3Client, l.bucketName, objectKey, logData)
 }
